4075d9651bd1e153c3d574d126d1167ba7f927d2
sato5extreme <sato5extreme@gmail.com>
2020-05-08 16:50:27 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,14 @@
+lock '3.14.0'
+set :application, 'chat-space'
+set :repo_url,  'git@github.com:sato5extreme/chat-space.git'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/chat-space.pem'] 
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
 set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
