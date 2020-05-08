e7e15dc2d4ced0463648d5a2bce84e76339c23ff
sato5extreme <sato5extreme@gmail.com>
2020-05-08 10:36:35 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-lock '2.1.6'
+lock '3.14.0'
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:sato5extreme/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
